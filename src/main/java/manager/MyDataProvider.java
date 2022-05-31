c7677c6ee01dee1bdb0684315cd9f18a961024d5
Dominus20 <102250448+Dominus20@users.noreply.github.com>
2022-05-31 20:56:08 +0300

    31.05.2022

---
+++ src/main/java/manager/MyDataProvider.java
@@ -0,0 +1,14 @@
+package manager;
+
+import org.testng.annotations.DataProvider;
+
+import java.util.Iterator;
+
+public class MyDataProvider {
+//    @DataProvider
+//    public Iterator<Object[]>fillLoginFormTest(){
+//
+//    }
+
+
+}
